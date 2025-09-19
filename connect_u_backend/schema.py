c7401fca39229cb53cb465d4c88a7a3ef6fbd047
austinmusuya <amusuya57@gmail.com>
2025-09-19 12:02:41 +0300

    feat(auth): integrate GraphQL auth schema with custom logout mutation
    - Added AuthMutation with user registration, login, account management, and JWT handling
    - Implemented LogoutUser mutation to revoke refresh tokens
    - Combined AuthUserQuery, UserQuery, and MeQuery into unified schema

---
+++ connect_u_backend/schema.py
@@ -0,0 +1,54 @@
+import graphene
+from graphql_auth.schema import UserQuery as AuthUserQuery, MeQuery
+from graphql_auth import mutations
+from graphql_jwt.refresh_token.models import RefreshToken
+from users.schema import UserQuery
+
+
+
+class LogoutUser(graphene.Mutation):
+    ok = graphene.Boolean()
+
+    class Arguments:
+        refresh_token = graphene.String(required=True)
+
+    def mutate(self, info, refresh_token, **kwargs):
+        try:
+            token = RefreshToken.objects.get(token=refresh_token)
+            token.revoke()  # blacklist token
+            return LogoutUser(ok=True)
+        except RefreshToken.DoesNotExist:
+            return LogoutUser(ok=False)
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
+    password_set = mutations.PasswordSet.Field() # For passwordless registration
+    password_change = mutations.PasswordChange.Field()
+    update_account = mutations.UpdateAccount.Field()
+    archive_account = mutations.ArchiveAccount.Field()
+    delete_account = mutations.DeleteAccount.Field()
+    send_secondary_email_activation =  mutations.SendSecondaryEmailActivation.Field()
+    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
+    swap_emails = mutations.SwapEmails.Field()
+    remove_secondary_email = mutations.RemoveSecondaryEmail.Field()
+
+    # django-graphql-jwt inheritances
+    token_auth = mutations.ObtainJSONWebToken.Field() # login
+    verify_token = mutations.VerifyToken.Field()
+    refresh_token = mutations.RefreshToken.Field()
+    revoke_token = mutations.RevokeToken.Field()
+    
+    # Custom logout mutation
+    logout_user = LogoutUser.Field()
+
+class Query(AuthUserQuery, UserQuery, MeQuery, graphene.ObjectType):
+    pass
+
+class Mutation(AuthMutation, graphene.ObjectType):
+   pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
